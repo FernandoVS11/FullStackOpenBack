6d2724cfd2f3b2152ba1dc749129ec32a3e3d005
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-06-06 19:46:23 -0600

    bug fix

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -66,10 +66,8 @@ app.post('/api/persons', (req, res) => {
 });
 
 const path = require('path')
+
 app.use(express.static(path.resolve(__dirname, 'dist')))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-})
 
 const PORT = 3001;
 app.listen(PORT, () => {
