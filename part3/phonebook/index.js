2dfd1f98cd895fd556628f76272f3ae38bb64e29
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-06-06 20:03:08 -0600

    3001

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -66,7 +66,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
