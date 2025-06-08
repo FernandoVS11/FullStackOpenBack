df83f47042d2558d1897524cb3fe62e2f680adf7
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-06-08 00:34:38 -0600

    feat: :sparkles: Implementation of DELETE and PUT. Also add error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Morgan personalizado para mostrar body en POST
 morgan.token('body', req => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -19,7 +20,16 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.post('/api/persons', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) res.json(person)
+      else res.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -33,19 +43,54 @@ app.post('/api/persons', (req, res) => {
 
   person.save()
     .then(savedPerson => res.json(savedPerson))
-    .catch(error => {
-      console.error(error)
-      res.status(500).json({ error: 'internal error saving person' })
-    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  const updatedPerson = {
+    name,
+    number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, updatedPerson, {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  })
+    .then(result => res.json(result))
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(error => next(error))
+})
+
+app.get('/info', (req, res, next) => {
   Person.countDocuments({}).then(count => {
     res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
-  })
+  }).catch(error => next(error))
 })
 
 app.use(express.static('dist'))
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
