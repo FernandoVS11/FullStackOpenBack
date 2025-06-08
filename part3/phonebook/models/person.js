edbc2f91c5c780fddaf621d31c01287fa1584ac5
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-06-08 01:11:20 -0600

    bug fix

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -7,9 +7,24 @@ mongoose.connect(url)
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.error('error connecting to MongoDB:', error.message))
 
+const phoneValidator = (number) => {
+  return /^\d{2,3}-\d+$/.test(number) && number.length >= 8
+}
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Name is required']
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: phoneValidator,
+      message: props => `${props.value} is not a valid phone number format (e.g. 09-1234556)`
+    },
+    required: [true, 'Phone number is required']
+  }
 })
 
 personSchema.set('toJSON', {
